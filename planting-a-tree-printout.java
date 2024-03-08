f937c9d8c6d3e3444e19481a32673a95c77c6673
Beregszászi István <beistvan@gmail.com>
2024-03-08 10:28:33 +0100

    Planting A Tree Print Out In Java

---
+++ planting-a-tree-printout.java
@@ -0,0 +1,36 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // I am István
+ 
+ 
+    /* Here will we print out 
+    a tree to the screen */
+
+    System.out.println("Hey there, I’m István and I’m learning to code in Java!");
+    System.out.println("Ready to get my hands dirty!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+
+    System.out.println();  
+
+    System.out.println("  * ** *  ");
+    System.out.println("  ******  ");
+    System.out.println("   ****   ");
+    System.out.println("    **    "); 
+    System.out.println("    **    "); 
+    System.out.println("~~~~~~~~~~");
+
+    System.out.println(); 
+
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("#######");
+  }
+}
