0a7ae267402e4b3c3f06faeb7a9d2e7817aa783b
Beregszászi István <beistvan@gmail.com>
2024-03-11 09:32:21 +0100

    My Profile - Declaring Variables In Java

---
+++ my-profile-declaring-variables-in-java.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+  public static void main(String[] args) {   
+    String name = "István";
+    int age = 20;
+    double desiredSalary = 85000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+  }
+}
