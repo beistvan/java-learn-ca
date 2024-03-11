910a8b4f380061ecb940edace048283ec5ea9b21
Beregszászi István <beistvan@gmail.com>
2024-03-11 09:21:29 +0100

    Char Variables - Example

---
+++ char-variables-example.java
@@ -0,0 +1,9 @@
+public class Char {
+	public static void main(String[] args) {   
+    char expectedGrade = 'A';
+    System.out.println(expectedGrade);
+    // char grade = 'A';
+    // char firstLetter = 'p';
+    // char punctuation = '!';
+	}
+}
