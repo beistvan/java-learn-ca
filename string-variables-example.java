54700fc16995fa1d3b43ef01f617043062ff89b5
Beregszászi István <beistvan@gmail.com>
2024-03-11 09:24:40 +0100

    String Variables - Example

---
+++ string-variables-example.java
@@ -0,0 +1,18 @@
+public class Song {
+  public static void main(String[] args) {   
+    String openingLyrics = "Yesterday, all my troubles seemed so far away";
+    System.out.println(openingLyrics);
+    // String greeting = "Hello World";
+    // String salutations = new String("Hello World");
+    // System.out.println("\"Hello World\"");
+    // Prints: "Hello World"
+    // System.out.println("This is the backslash symbol: \\");
+    // Prints: This is the backslash symbol: \
+    // System.out.println("Hello\nGoodbye");
+    /*
+    Prints:
+    Hello
+    Goodbye
+    */
+  }
+}
