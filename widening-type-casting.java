890a27eea01bcec1b1dcc48be63471c4f7cbd8ec
Beregszászi István <beistvan@gmail.com>
2024-04-01 14:24:38 +0200

    Widening Type Casting

---
+++ widening-type-casting.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main (String[]args)
+	{
+		int numInt = 9;
+		System.out.println ("Int value: " + numInt); // 9
+		// convert an int to a double
+		System.out.println ("Double value: " + (double) numInt); // 9.0
+	}
+}
