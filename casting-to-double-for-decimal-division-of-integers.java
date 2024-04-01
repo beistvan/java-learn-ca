e661855f48450bdfd1d7df45ef89c81bc1fb5f67
Beregszászi István <beistvan@gmail.com>
2024-04-01 14:28:37 +0200

    Casting To Double For Decimal Division Of Integers

---
+++ casting-to-double-for-decimal-division-of-integers.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main (String[]args)
+	{
+		System.out.println (100.0 / 5);	// Prints: 20.0
+		System.out.println ((double) (100 / 5));	// Prints: 20.0
+	}
+}
