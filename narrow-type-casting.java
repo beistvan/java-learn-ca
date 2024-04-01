10ceda716f460687e89311c79aaf9447ce0cc968
Beregszászi István <beistvan@gmail.com>
2024-04-01 14:12:25 +0200

    Narrow Type Casting

---
+++ narrow-type-casting.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main (String[]args)
+	{
+		double numDouble = 12.99;
+		System.out.println ("Double value: " + numDouble); // 12.99
+		// convert a double to int - Narrow Type Casting
+		int numInt = (int) numDouble;
+		System.out.println ("Int value: " + numInt); // 12
+	}
+}
