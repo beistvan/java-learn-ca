1d335258cb638ed26b28c11c384d930e192ada6e
Beregszászi István <beistvan@gmail.com>
2024-04-01 14:18:44 +0200

    Integer Min And Max Values

---
+++ integer-min-and-max-values.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main (String[]args)
+	{
+		System.out.println ("Min: " + Integer.MIN_VALUE);	// Prints: Min: -2147483648
+		System.out.println ("Max: " + Integer.MAX_VALUE);	// Prints: Max: 2147483647
+	}
+}
+
