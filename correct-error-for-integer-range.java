0f5aec2afee36d4934137d17429f9ca4c5f9cbc0
Beregszászi István <beistvan@gmail.com>
2024-04-01 14:21:55 +0200

    Correct Error For Integer Range

---
+++ correct-error-for-integer-range.java
@@ -0,0 +1,11 @@
+class IntegerRange {
+	public static void main(String[] args) {
+		// the variable a is declared with a value outside the range
+		//int a = 3147483647;
+		//System.out.println(a);
+
+		// the sum of the following expression is outside the appropriate int range
+		int b = 2147483645 + 3;
+		System.out.println(b);
+	}
+}
