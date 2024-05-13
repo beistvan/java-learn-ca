1e3261bf21f1e3fcefc1bde8f4584e970b3637ec
Beregszászi István <beistvan@gmail.com>
2024-05-13 18:51:11 +0200

    Java Arrays Example

---
+++ java-arrays-example.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Main {
+  public static void main(String[] args){
+      String[] students = {"Sade", "Alexus", "Sam", "Koma"}; 
+      int[] mathScores = new int[4];
+      mathScores[0] = 64;
+      mathScores[1] = 57;
+      mathScores[2] = 76;
+      mathScores[3] = 98;
+      System.out.println(mathScores[0]);
+      System.out.println(mathScores[1]);
+      System.out.println(mathScores[2]);
+      System.out.println(mathScores[3]);
+  }
+}
