305b6df0fd6181fe76e6a0b25077998d6fb22c2f
Beregszászi István <beistvan@gmail.com>
2024-05-26 08:35:23 +0200

    Break Statement In Java

---
+++ break-statement-in-java.java
@@ -0,0 +1,10 @@
+class Numbers {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+      if (i == 4) {
+        break;
+      }
+    }
+  }
+}
