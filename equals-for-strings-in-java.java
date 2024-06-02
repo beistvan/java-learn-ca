8f3f17b59b1ae7f82b4f41dd56d678b46ac4f62a
Beregszászi István <beistvan@gmail.com>
2024-06-02 08:38:50 +0200

    Equals For Strings In Java

---
+++ equals-for-strings-in-java.java
@@ -0,0 +1,13 @@
+public class Password {
+  
+  public static void main(String[] args) {
+    
+    String password = "correcthorsebatterystaple";
+    
+    // Write the code:
+
+    System.out.println(password.equals("Tr0ub4dor&3"));
+    
+  }
+  
+}
