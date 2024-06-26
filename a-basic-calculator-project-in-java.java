db9e925d9a019d9de06f7a5adfbae58394a8378c
Beregszászi István <beistvan@gmail.com>
2024-06-26 10:37:40 +0200

    A Basic Calculator Project In Java

---
+++ a-basic-calculator-project-in-java.java
@@ -0,0 +1,29 @@
+/*
+This is an Abacus that is used to slide beads around and do math.
+It was created by Istvan in 2024.
+*/
+public class Calculator{
+  public Calculator(){
+ 
+  }  
+  public int add(int a, int b){
+    return a + b;
+  }
+  public int subtract(int a, int b){
+    return a - b;
+  }
+  public int multiply(int a, int b){
+    return a * b;
+  }
+  public int divide(int a, int b){
+    return a / b;
+  }
+  public int modulo(int a, int b){
+    return a % b;
+  }
+  public static void main(String[] args){
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+    System.out.println(myCalculator.subtract(45, 11));
+  }
+}
